0e1dfb6646e3d36f4f47b2cd3351cdfe0a82a0cb
ngdo-pro <ngdo27@gmail.com>
2017-01-04 18:36:18 +0100

    User Entity updated and migration for Baptem and User done

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -25,5 +25,323 @@ class User extends BaseUser
     {
         return $this->id;
     }
-}
+    /**
+     * @var string
+     */
+    private $firstName;
 
+    /**
+     * @var string
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     */
+    private $slug;
+
+    /**
+     * @var \DateTime
+     */
+    private $birthDate;
+
+    /**
+     * @var string
+     */
+    private $biography;
+
+    /**
+     * @var string
+     */
+    private $signatureDish;
+
+    /**
+     * @var float
+     */
+    private $rating;
+
+    /**
+     * @var string
+     */
+    private $phone;
+
+    /**
+     * @var integer
+     */
+    private $level;
+
+    /**
+     * @var integer
+     */
+    private $participation;
+
+    /**
+     * @var string
+     */
+    private $profession;
+
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return User
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param \DateTime $birthDate
+     *
+     * @return User
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set biography
+     *
+     * @param string $biography
+     *
+     * @return User
+     */
+    public function setBiography($biography)
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    /**
+     * Get biography
+     *
+     * @return string
+     */
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+
+    /**
+     * Set signatureDish
+     *
+     * @param string $signatureDish
+     *
+     * @return User
+     */
+    public function setSignatureDish($signatureDish)
+    {
+        $this->signatureDish = $signatureDish;
+
+        return $this;
+    }
+
+    /**
+     * Get signatureDish
+     *
+     * @return string
+     */
+    public function getSignatureDish()
+    {
+        return $this->signatureDish;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param float $rating
+     *
+     * @return User
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return float
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     *
+     * @return User
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set participation
+     *
+     * @param integer $participation
+     *
+     * @return User
+     */
+    public function setParticipation($participation)
+    {
+        $this->participation = $participation;
+
+        return $this;
+    }
+
+    /**
+     * Get participation
+     *
+     * @return integer
+     */
+    public function getParticipation()
+    {
+        return $this->participation;
+    }
+
+    /**
+     * Set profession
+     *
+     * @param string $profession
+     *
+     * @return User
+     */
+    public function setProfession($profession)
+    {
+        $this->profession = $profession;
+
+        return $this;
+    }
+
+    /**
+     * Get profession
+     *
+     * @return string
+     */
+    public function getProfession()
+    {
+        return $this->profession;
+    }
+}
