1c81048eaecb47674507a44e2d17898c51d79ac6
ngdo-pro <ngdo27@gmail.com>
2017-01-05 07:43:30 +0100

    relationship between Baptem and Payment created, migrations done

--- src/AppBundle/Entity/Baptem.php
+++ src/AppBundle/Entity/Baptem.php
@@ -109,4 +109,79 @@ class Baptem
     {
         return $this->places;
     }
+    /**
+     * @var \AppBundle\Entity\Service
+     */
+    private $service;
+
+
+    /**
+     * Set service
+     *
+     * @param \AppBundle\Entity\Service $service
+     *
+     * @return Baptem
+     */
+    public function setService(\AppBundle\Entity\Service $service = null)
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    /**
+     * Get service
+     *
+     * @return \AppBundle\Entity\Service
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $payments;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->payments = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add payment
+     *
+     * @param \AppBundle\Entity\Payment $payment
+     *
+     * @return Baptem
+     */
+    public function addPayment(\AppBundle\Entity\Payment $payment)
+    {
+        $this->payments[] = $payment;
+
+        return $this;
+    }
+
+    /**
+     * Remove payment
+     *
+     * @param \AppBundle\Entity\Payment $payment
+     */
+    public function removePayment(\AppBundle\Entity\Payment $payment)
+    {
+        $this->payments->removeElement($payment);
+    }
+
+    /**
+     * Get payments
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPayments()
+    {
+        return $this->payments;
+    }
 }
--- src/AppBundle/Entity/FoodType.php~
+++ src/AppBundle/Entity/FoodType.php~
@@ -52,4 +52,3 @@ class FoodType
         return $this->name;
     }
 }
-
