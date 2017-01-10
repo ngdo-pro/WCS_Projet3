919a746cb1ed8a47f9f56546529f0fc1c66e1540
ngdo-pro <ngdo27@gmail.com>
2017-01-10 06:08:17 +0100

    finished treatment of purchaseAction in BaptismController, only left redirection

--- src/AppBundle/Controller/BaptismController.php
+++ src/AppBundle/Controller/BaptismController.php
@@ -11,9 +11,9 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Baptism;
 use AppBundle\Entity\BaptismHasUser;
+use AppBundle\Entity\Payment;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use UserBundle\Entity\User;
 
@@ -21,7 +21,7 @@ class BaptismController extends Controller
 {
 
     public function selectAction(Request $request){
-        // TODO: This is a fake function, need to be replaced with the real searchAction
+        // TODO: This is a fake function, need to be replaced with the real selectAction
         $form = $this->createFormBuilder()
             ->add("send", SubmitType::class)
             ->getForm();
@@ -44,6 +44,16 @@ class BaptismController extends Controller
         ));
     }
 
+    /**
+     * This action receive the chosen baptism parameters, and has 2 parts :
+     *     - First, it sends to the view the parameters and a form asking for confirmation
+     *     - Second, if confirmation is received, it creates pending baptism, pending payment and
+     *       baptismHasUser, then, it redirects to sogenactif payment page.
+     *
+     * @param Request $request
+     * @param Baptism $baptism
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function purchaseAction(Request $request, Baptism $baptism)
     {
         $form = $this->createFormBuilder()
@@ -53,14 +63,31 @@ class BaptismController extends Controller
 
         if($form->isSubmitted() && $form->isValid()){
             $em = $this->getDoctrine()->getManager();
+
+            $baptism->setStatus("pending");
+            $em->persist($baptism);
+
             /** @var User $user */
             $user = $this->getUser();
+
+            /** creation of BaptismHasUser */
             $baptismHasUser = new BaptismHasUser();
             $baptismHasUser->setBaptism($baptism);
             $baptismHasUser->setUser($user);
-            $baptismHasUser->setRole("baptised");
-            //$em->persist($baptism);
-            //$em->flush();
+            $baptismHasUser->setRole(true);
+            $em->persist($baptismHasUser);
+
+            $payment = new Payment();
+            $payment->setFirstName($user->getFirstName());
+            $payment->setLastName($user->getLastName());
+            $payment->setProductName("baptism");
+            $payment->setStatus("pending");
+            $payment->setConfirmationSent(false);
+            $payment->setBaptismHasUser($baptismHasUser);
+            $payment->setUser($user);
+            $em->persist($payment);
+
+            $em->flush();
         }
 
         return $this->render('app/baptism/purchase.html.twig', array(
