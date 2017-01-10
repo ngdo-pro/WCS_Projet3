cec526ab26212dd18fd9fba380325f4f1c758e21
damien <dbeauseigneur@gmail.com>
2017-01-10 15:00:44 +0100

    cleaning for death code and maj to send infot to baptism purchase by using the symfony session. Nb the code for the purchase is just here for testing.

--- src/AppBundle/Controller/BaptismController.php
+++ src/AppBundle/Controller/BaptismController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Baptism;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use DateTime;
 
 /**
  * Baptism controller.
@@ -12,115 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class BaptismController extends Controller
 {
-    /**
-     * Lists all baptism entities.
-     *
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $baptisms = $em->getRepository('AppBundle:Baptism')->findAll();
-
-        return $this->render('baptism/index.html.twig', array(
-            'baptisms' => $baptisms,
-        ));
-    }
-
-    /**
-     * Creates a new baptism entity.
-     *
-     */
-    public function newAction(Request $request)
-    {
-        $baptism = new Baptism();
-        $form = $this->createForm('AppBundle\Form\BaptismType', $baptism);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($baptism);
-            $em->flush($baptism);
-
-            return $this->redirectToRoute('baptism_info_show', array('id' => $baptism->getId()));
-        }
-
-        return $this->render('baptism/new.html.twig', array(
-            'baptism' => $baptism,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a baptism entity.
-     *
-     */
-    public function showAction(Baptism $baptism)
-    {
-        $deleteForm = $this->createDeleteForm($baptism);
-
-        return $this->render('baptism/show.html.twig', array(
-            'baptism' => $baptism,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing baptism entity.
-     *
-     */
-    public function editAction(Request $request, Baptism $baptism)
-    {
-        $deleteForm = $this->createDeleteForm($baptism);
-        $editForm = $this->createForm('AppBundle\Form\BaptismType', $baptism);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('baptism_info_edit', array('id' => $baptism->getId()));
-        }
-
-        return $this->render('baptism/edit.html.twig', array(
-            'baptism' => $baptism,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a baptism entity.
-     *
-     */
-    public function deleteAction(Request $request, Baptism $baptism)
-    {
-        $form = $this->createDeleteForm($baptism);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($baptism);
-            $em->flush($baptism);
-        }
-
-        return $this->redirectToRoute('baptism_info_index');
-    }
-
-    /**
-     * Creates a form to delete a baptism entity.
-     *
-     * @param Baptism $baptism The baptism entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Baptism $baptism)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('baptism_info_delete', array('id' => $baptism->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
 
     /**
      * @param Request $request
@@ -128,15 +20,28 @@ class BaptismController extends Controller
      */
     public function SelectAction(Request $request)
     {
-        $baptism = new Baptism();
-        $editForm = $this->createForm('AppBundle\Form\BaptismType', $baptism);
+
+        $requestC = $request->request;
         $em = $this->getDoctrine()->getManager();
+        if ($requestC->get('origin','')=='select') {
+            $baptism = array();
+            $baptism['id'] = $requestC->get('id');
+            $baptism['date'] = $requestC->get('date');
+            $baptism['status'] = $requestC->get('status');
+            $baptism['places'] = $requestC->get('places');
+            $baptism['restaurant_id']=$requestC->get('restaurant_id');
+            $baptism['service']=$requestC->get('service_id');
+
+            $session = $request->getSession();
+            $session->set('baptism',$baptism);
+            return $this->redirectToRoute('baptism_purchase') ;
+        }
+
 
         $baptisms = $em->getRepository('AppBundle:Baptism')->findAll();
 
         return $this->render('baptism/select.html.twig', array(
             'baptisms' => $baptisms,
-            'edit_form' => $editForm->createView(),
         ));
 
     }
@@ -147,12 +52,14 @@ class BaptismController extends Controller
      */
     public function purchaseAction(Request $request)
     {
-        //$deleteForm = $this->createDeleteForm($baptism);
+        $session = $request->getSession();
+        $baptismArray = $session->get('baptism');
+        var_dump($baptismArray);
         $baptism = new Baptism();
+
 
         return $this->render('baptism/show.html.twig', array(
             'baptism' => $baptism,
-            //'delete_form' => $deleteForm->createView(),
         ));
     }
 }
