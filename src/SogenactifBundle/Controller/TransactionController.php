3eeef1fb780861ada74eeb15990066efc5ad9185
ngdo-pro <ngdo27@gmail.com>
2017-01-10 07:40:28 +0100

    created TransactionController with sendingAction

--- src/SogenactifBundle/Controller/TransactionController.php
+++ src/SogenactifBundle/Controller/TransactionController.php
@@ -9,7 +9,13 @@
 namespace SogenactifBundle\Controller;
 
 
-class TransactionController
-{
+use AppBundle\Entity\Price;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UserBundle\Entity\User;
 
+class TransactionController extends Controller
+{
+    public function sendingAction(Price $price, User $user){
+
+    }
 }
