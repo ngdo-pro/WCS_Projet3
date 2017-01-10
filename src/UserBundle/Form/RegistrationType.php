39e990be5c44a7337ffc631c68232657ec271a07
Nagx <k.javazzo@gmail.com>
2017-01-10 11:23:48 +0100

    create translation in fr and en

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -13,7 +13,7 @@ class RegistrationType extends AbstractType
     {
         $builder
             ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
-            ->add('username', null, array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
             ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
                 'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
                 'options' => array('translation_domain' => 'FOSUserBundle'),
@@ -21,13 +21,13 @@ class RegistrationType extends AbstractType
                 'second_options' => array('label' => 'form.password_confirmation'),
                 'invalid_message' => 'fos_user.password.mismatch',
             ))
-            ->add('first_name')
-            ->add('last_name')
-            ->add('birth_date')
-            ->add('biography')
-            ->add('signature_dish')
-            ->add('phone')
-            ->add('profession')
+            ->add('first_name', null, array('label' => 'form.first_name', 'translation_domain' => 'FOSUserBundle'))
+            ->add('last_name', null, array('label' => 'form.last_name', 'translation_domain' => 'FOSUserBundle'))
+            ->add('birth_date', null, array('label' => 'form.birth_date', 'translation_domain' => 'FOSUserBundle'))
+            ->add('biography', null, array('label' => 'form.biography', 'translation_domain' => 'FOSUserBundle'))
+            ->add('signature_dish', null, array('label' => 'form.signature_dish', 'translation_domain' => 'FOSUserBundle'))
+            ->add('phone', null, array('label' => 'form.phone', 'translation_domain' => 'FOSUserBundle'))
+            ->add('profession', null, array('label' => 'form.profession', 'translation_domain' => 'FOSUserBundle'))
         ;
     }
 
