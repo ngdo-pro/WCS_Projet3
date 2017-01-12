22ddfe386045c045e859e153dd1eaf2beac5fe07
ngdo-pro <ngdo27@gmail.com>
2017-01-12 20:34:46 +0100

    commented BaptismHasUserRepository

--- src/AppBundle/Repository/BaptismHasUserRepository.php
+++ src/AppBundle/Repository/BaptismHasUserRepository.php
@@ -12,6 +12,13 @@ use AppBundle\Entity\Baptism;
  */
 class BaptismHasUserRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * this function find out if there are other people on this baptism
+     * allowing us to know if we have to remove baptism in case of
+     * cancelled payment
+     * @param Baptism $baptism
+     * @return array
+     */
     public function findOtherByBaptism(Baptism $baptism){
         return $this->createQueryBuilder('bhu')
             ->select('COUNT(bhu.id)')
