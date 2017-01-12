15082b3800720754198765a1cd50fee30d9ef9d0
damien <dbeauseigneur@gmail.com>
2017-01-12 12:01:50 +0100

    adding findAllFree in BaptismRepository return SQL struct for the result

--- src/AppBundle/Repository/BaptismRepository.php
+++ src/AppBundle/Repository/BaptismRepository.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Repository;
 
+use datetime;
 /**
  * BaptemRepository
  *
@@ -10,24 +11,111 @@ namespace AppBundle\Repository;
  */
 class BaptismRepository extends \Doctrine\ORM\EntityRepository
 {
-    public function findAllFree($city,$restaurant,$nb,$baptismDate,$service){
 
-        $select = " SELECT b.id, service_id, status, date, places , b.restaurant_id ";
+    /**
+     * function findAllFree
+     * Object: generate the query for the search for free baptism date
+     * @param $city
+     * @param $restaurant
+     * @param $nb
+     * @param $baptismDate
+     * @param $service
+     */
+    public function findAllFree($city, $restaurant, $nb, $baptismDate, $service){
+
+        $select = " SELECT b.id, b.service_id, b.status, b.date, b.places , b.restaurant_id ";
+        $select1 = " SELECT b.restaurant_id ";
         $from = " FROM baptism b ";
         if (is_null($nb)) {
             $nb = 0;
         }
         $where = " WHERE (places -$nb)>= 0 ";
 
+        $baptismCityInfo = false;
         if (! is_null($city)) {
             $from .= " INNER JOIN restaurant r on r.id= b.restaurant_id ";
             $from .= " INNER JOIN city c on c.name= r.city or c.postal_code = r.postal_code ";
-            $where .= " AND r.city = '$city'" ;
+            $where .= " AND r.city = :city " ;
+            $baptismCityInfo = true;
+        }
+
+        $baptismRestaurantInfo = false;
+        if (! is_null($restaurant)) {
+            if  (!$baptismCityInfo) {
+                $from .= " INNER JOIN restaurant r on r.id= b.restaurant_id ";
+            }
+            $where .= " AND r.name = :restaurant ";
+            $baptismRestaurantInfo = true;
+        }
+
+        $now = new DateTime();
+        $now->setTime(0, 0, 0);
+        $baptismDateInfo = false;
+        if (! is_null($baptismDate)) {
+            $startDate = DateTime::createFromFormat('Y-m-d', $baptismDate);
+            if ($startDate instanceof DateTime) {
+                $startDate->setTime(0, 0, 0);
+            }
+            if (!$startDate || $startDate < $now) {
+                $startDate = $now;
+            }
+            $where .= " AND date = :baptismdate ";
+            // we must use parameters to set this correctly this data in the query
+            $baptismDateInfo = true;
+        }
+
+        $baptismServiceInfo = false;
+        if (!is_null($service)) {
+            $where .= " AND service_id = $service";
+            $baptismServiceInfo = true;
         }
 
 
+        $select2 ="";
+        $from2 = "";
+        $where2="";
+        if ($baptismDateInfo && $baptismServiceInfo) {
+            $select2 = " SELECT -1 so.id, so.service_id, 'Pending', :baptismdate, ifnull(soe.status,case weekday(:baptismdate) ";
+            $select2 .= " when 0 then monday ";
+            $select2 .= " when 1 then tuesday ";
+            $select2 .= " when 2 then wednesday ";
+            $select2 .= " when 3 then thursday ";
+            $select2 .= " when 4 then friday ";
+            $select2 .= " when 5 then saturday ";
+            $select2 .= " when 6 then sunday ";
+            $select2 .= " end case as places, restaurant_id ";
+            $from2 = " FROM service_opening so ";
+            $from2 .= " LEFT JOIN service_opening_exception soe on (soe.service_id = so.service_id ";
+            $from2 .= " and soe.restaurant_id = so.restaurant_id ";
+            $from2 .= " and soe.date = :baptismdate ";
+            $where2 = " WHERE service_id = $service ";
+            $where2 .= " AND (case weekday(:baptismdate) ";
+            $where2 .= " when 0 then monday ";
+            $where2 .= " when 1 then tuesday ";
+            $where2 .= " when 2 then wednesday ";
+            $where2 .= " when 3 then thursday ";
+            $where2 .= " when 4 then friday ";
+            $where2 .= " when 5 then saturday ";
+            $where2 .= " when 6 then sunday ";
+            $where2 .= " end case) > $nb ";
+            $where2 .= " so.restaurant_id not in ( ";
+            $where2 .= $select1 . $from . $where . ")";
 
-        $select2 = " SELECT -1 id, service_id, 'Pending', :date, 3, restaurant_id ";
-        $from2 = " FROM service_opening so ";
+            if ($baptismCityInfo) {
+                $from2 .= " INNER JOIN restaurant r on r.id= so.restaurant_id ";
+                $from2 .= " INNER JOIN city c on c.name= r.city or c.postal_code = r.postal_code ";
+                $where2 .= " AND r.city = :city " ;
+            }
+
+            if ($baptismRestaurantInfo) {
+                if  (!$baptismCityInfo) {
+                    $from2 .= " INNER JOIN restaurant r on r.id= so.restaurant_id ";
+                }
+                $where2 .= " AND r.name = :restaurant ";
+            }
+            return $select . $from . $where . " union " . $select2 . $from2 . $where2 . ";";
+        }
+
+
     }
 }
