9b2f8abbfffefc996153f86c0eb8687426daada8
Nagx <k.javazzo@gmail.com>
2017-01-16 00:43:06 +0100

    add regex rulesfor format value  before fletch into database

--- src/UserBundle/EventListener/UserRegistrationListener.php
+++ src/UserBundle/EventListener/UserRegistrationListener.php
@@ -43,8 +43,36 @@ class UserRegistrationListener implements EventSubscriberInterface
         } else if ($gender == 'f'){
             $user->setCivility(false);
         }
+        // Here we take a string to set it Uppercase the first character
+        $firstName = ucwords(strtolower($user->getFirstName()));
+        $user->setFirstName($firstName);
+
+        $lastName = ucwords(strtolower($user->getLastName()));
+        $user->setLastName($lastName);
+
+        $city = ucwords(strtolower($user->getCity()));
+        $user->setCity($city);
+
         // Here we set the email value to the username for login with email
         $user->setUsername($user->getEmail());
+
+        // Here we normalize the phone number
+        $phone = $user->getMobilePhone();
+        if (strlen($phone) > 10){
+            $phone = preg_replace('/[. ]/', "-", $phone);
+            $user->setMobilePhone($phone);
+        } else if (strlen($phone) == 10){
+            $i = 0;
+            $mobile = "";
+            while ($i != 10){
+                $nb = substr($phone, $i, 2);
+                $mobile .= $nb.'-';
+                $i = $i + 2;
+            }
+            $phone = preg_replace('/[-]$/', "", $mobile);
+            $user->setMobilePhone($phone);
+        }
+
         // Here we create an user slug with UserSlugService
         $slug = $this->container->get('user.new_user_slug');
         $userSlug = $slug->setNewUserSlug($user->getFirstName(), $user->getLastName());
