ba95c93972d06f7138938c781971ffef6955c650
Nagx <k.javazzo@gmail.com>
2017-01-15 23:42:06 +0100

    fix form bug and add Regex rules for form validation

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -2,13 +2,14 @@
 
 namespace UserBundle\Form;
 
-use FOS\UserBundle\Util\LegacyFormHelper;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
@@ -21,24 +22,29 @@ class RegistrationType extends AbstractType
                 'm' => 'form.mister',
                 'f' => 'form.misses'),
                 'expanded' => true,
-                'multiple' => false
-            ))
-            ->add('address', null, array('label' => 'form.address', 'translation_domain' => 'FOSUserBundle'))
-            ->add('city', null, array('label' => 'form.city', 'translation_domain' => 'FOSUserBundle'))
-            ->add('zip_code', NumberType::class, array('label' => 'form.zip_code', 'translation_domain' => 'FOSUserBundle'))
-            ->add('first_name', null, array('label' => 'form.first_name', 'translation_domain' => 'FOSUserBundle','required' => true,))
-            ->add('last_name', null, array('label' => 'form.last_name', 'translation_domain' => 'FOSUserBundle','required' => true,))
+                'multiple' => false,
+                ))
+            ->add('address', null, array('label' => 'form.address', 'translation_domain' => 'FOSUserBundle', 'trim' => true))
+            ->add('city', null, array('label' => 'form.city', 'translation_domain' => 'FOSUserBundle', 'trim' => true))
+            ->add('zip_code', null, array('label' => 'form.zip_code', 'translation_domain' => 'FOSUserBundle', 'trim' => true))
+            ->add('first_name', null, array('label' => 'form.first_name', 'translation_domain' => 'FOSUserBundle','required' => true, 'trim' => true))
+            ->add('last_name', null, array('label' => 'form.last_name', 'translation_domain' => 'FOSUserBundle','required' => true, 'trim' => true))
             ->add('birth_date', DateType::class, array('label' => 'form.birth_date', 'translation_domain' => 'FOSUserBundle', 'widget' => 'choice',
                 'years' => range(date('Y')-18,date('Y')-80)))
             ->add('mobile_phone', null, array('label' => 'form.phone', 'translation_domain' => 'FOSUserBundle'))
             ->add('checkbox', CheckboxType::class, array('translation_domain' => 'FOSUserBundle',
                 'label' => 'form.checkbox',
                 'required' => true,
-                'mapped' => false))
-            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+                'mapped' => false,
+                ))
+            ->add('email', EmailType::class, array(
+                'label' => 'form.email',
+                'translation_domain' => 'FOSUserBundle',
+                'trim' => true,
+                ))
             ->add('username', HiddenType::class, array('data' => 'NewUser'))
-            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
                 'options' => array('translation_domain' => 'FOSUserBundle'),
                 'first_options' => array('label' => 'form.password'),
                 'second_options' => array('label' => 'form.password_confirmation'),
@@ -60,6 +66,6 @@ class RegistrationType extends AbstractType
 
     public function getParent()
     {
-        return 'fos_user_registration';
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
 }
