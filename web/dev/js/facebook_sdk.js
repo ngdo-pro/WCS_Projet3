3ea99b0b1594a015be9b463cdb0a6bb930110037
ngdo-pro <ngdo27@gmail.com>
2017-01-19 05:29:00 +0100

    added FB.init

--- web/dev/js/facebook_sdk.js
+++ web/dev/js/facebook_sdk.js
@@ -1,3 +1,9 @@
+FB.init({
+    appId      : '{your-app-id}',
+    status     : true,
+    xfbml      : true,
+    version    : 'v2.7' // or v2.6, v2.5, v2.4, v2.3
+});
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
