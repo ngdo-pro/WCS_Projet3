da0ce0887ea5e3514de143198a1c5451793443be
damien <dbeauseigneur@gmail.com>
2017-01-27 15:12:35 +0100

    try inserting google API

--- src/AppBundle/Controller/RestaurantController.php
+++ src/AppBundle/Controller/RestaurantController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ivory\GoogleMap\Base\Bound;
 use Ivory\GoogleMap\Base\Coordinate;
 use AppBundle\Entity\Restaurant;
+use Ivory\GoogleMapBundle\Twig\GoogleMapExtension;
 
 class RestaurantController extends Controller
 {
@@ -27,7 +28,130 @@ class RestaurantController extends Controller
         $map->setAutoZoom(true);
         // without this Option the size is limited 300px width and 300 px height
         $map->setStylesheetOption('width', '100%');
-        $map->setStylesheetOption('height', '500px');
+        $map->setStylesheetOption('height', '450px');
+
+        // not working
+        /*$styles = array (
+            array( "elementType"=> "geometry",
+                "stylers"=> array (
+                    "color"=> "#f5f5f5"
+                )
+            ),
+            array( "elementType"=> "labels.icon",
+                "stylers"=> array (
+                    "visibility"=>"off"
+                )
+            ),
+            array( "elementType"=> "labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#616161"
+                )
+            ),
+            array( "elementType"=> "labels.text.stroke",
+                "stylers"=> array (
+                    "color"=> "#f5f5f5"
+                )
+            ),
+            array(
+                "featureType" =>"administrative.land_parcel",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#bdbdbd"
+                )
+            ),
+            array(
+                "featureType" =>"poi",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#eeeeee"
+                )
+            ),
+            array(
+                "featureType" =>"poi",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#757575"
+                )
+            ),
+            array(
+                "featureType" =>"poi.park",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#e5e5e5"
+                )
+            ),
+            array(
+                "featureType" =>"poi.park",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#9e9e9e"
+                )
+            ),
+            array(
+                "featureType" =>"road",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#ffffff"
+                )
+            ),
+            array(
+                "featureType" =>"road.arterial",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#757575"
+                )
+            ),
+            array(
+                "featureType" =>"road.highway",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#dadada"
+                )
+            ),
+            array(
+                "featureType" =>"road.highway",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#616161"
+                )
+            ),
+            array(
+                "featureType" =>"road.local",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#9e9e9e"
+                )
+            ),
+            array(
+                "featureType"=>"transit.line",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#e5e5e5"
+                )
+            ),
+            array(
+                "featureType"=>"transit.station",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#eeeeee"
+                )
+            ),
+            array("featureType"=>"water",
+                "elementType"=>"geometry",
+                "stylers"=> array (
+                    "color"=> "#c9c9c9"
+                )
+            ),
+            array("featureType"=>"water",
+                "elementType"=>"labels.text.fill",
+                "stylers"=> array (
+                    "color"=> "#9e9e9e"
+                )
+            ),
+        );
+
+
+        $map->setMapOptions(array ("styles"=> $styles)); */
 
         foreach ($restaurants as $restaurant){
             $marker = new Marker();
